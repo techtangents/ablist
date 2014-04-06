e9a9c4cb942980a51b0b13460c9b9992e61d2f74
techtangents <dylan@techtangents.com>
2014-04-07 06:17:30 +1000

    initial

---
+++ src/Data/ABList.hs
@@ -0,0 +1,95 @@
+module Data.ABList (
+  ABList(..),
+  abFoldr,
+  abFoldr',
+  abToListEither,
+  abFromListEither,
+  aaToList,
+  aaFromList,
+  abHead,
+  abTail
+) where
+
+infixr 5 :/
+
+data ABList a b = ABNil | a :/ ABList b a
+  deriving (Eq, Ord, Show)
+
+abFoldr :: ((Either a b) -> t -> t) -> t -> (ABList a b) -> t
+abFoldr = undefined
+
+abFoldr' :: (a -> t -> t) -> (b -> t -> t) -> t -> (ABList a b) -> t
+abFoldr' = undefined
+
+abToListEither :: ABList a b -> [Either a b]
+abToListEither ABNil = []
+abToListEither (a :/ ABNil) = [Left a]
+abToListEither (a :/ b :/ abz) = (Left a) : (Right b) : abToListEither abz
+
+-- either backwards
+rehtie :: Either a b -> Either b a
+rehtie = either Right Left
+
+rehties :: [Either a b] -> [Either b a]
+rehties = fmap rehtie
+
+abFromListEither :: [Either a b] -> Maybe (ABList a b)
+abFromListEither [] = Just ABNil
+abFromListEither (Left a : as) = fmap (a :/) (abFromListEither . rehties $ as)
+abFromListEither (Right _ : _) = Nothing
+
+aaToList :: ABList a a -> [a]
+aaToList = undefined
+
+aaFromList :: [a] -> ABList a a
+aaFromList = undefined
+
+abHead :: ABList a b -> Maybe a
+abHead = undefined
+
+abTail :: ABList a b -> Maybe (ABList b a)
+abTail = undefined
+
+abInit :: ABList a b -> Maybe (ABList a b)
+abInit = undefined
+
+abZip :: [a] -> [b] -> ABList a b
+abZip = undefined
+
+abZipPairs :: [(a, b)] -> ABList a b
+abZipPairs = undefined
+
+abMap :: (a -> a') -> (b -> b') -> ABList a b -> ABList a' b'
+abMap = undefined
+
+abMapToList :: (a -> t) -> (b -> t) -> ABList a b -> [t]
+abMapToList = undefined
+
+abMapToList' :: (Either a b -> t) -> ABList a b -> [t]
+abMapToList' = undefined
+
+abUnzip :: ABList a b -> ([a], [b])
+abUnzip = undefined
+
+abUnzip' :: ABList a b -> (a, [b], [a])
+abUnzip' = undefined
+
+abUnzip'' :: ABList a b -> (a, [(b, a)])
+abUnzip'' = undefined
+
+abReverse :: ABList a b -> Either (ABList a b) (ABList b a)
+abReverse = undefined
+
+abMapLefts :: (a -> a') -> ABList a b -> ABList a' b
+abMapLefts = undefined
+
+abMapRights :: (b -> b') -> ABList a b -> ABList a b'
+abMapRights = undefined
+
+
+-- right-biased functor
+instance Functor (ABList a) where
+  fmap = abMapRights
+
+
+
---
+++ test/Tests.hs
@@ -0,0 +1,85 @@
+module Main where
+
+import Test.Framework
+import Test.Framework.Providers.QuickCheck2
+import Test.Framework.Providers.HUnit
+import Test.QuickCheck
+
+import Data.ABList
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: [Test]
+tests =
+  [
+    g_abToListEither,
+    g_abFromListEither
+  ]
+
+g_abToListEither =
+  testGroup "abToListEither"
+  [ testProperty "empty" prop_abToListEither_Empty
+  , testProperty "1" prop_abToListEither_1
+  , testProperty "2" prop_abToListEither_2
+  , testProperty "3" prop_abToListEither_3
+  , testProperty "4" prop_abToListEither_4
+  ]
+
+prop_abToListEither_Empty :: Bool
+prop_abToListEither_Empty = (abToListEither ABNil) == ([] :: [Either String Int])
+
+prop_abToListEither_1 :: Int -> Bool
+prop_abToListEither_1 a = (abToListEither $ a :/ ABNil) == ([Left a] :: [Either Int String])
+
+prop_abToListEither_2 :: Int -> String -> Bool
+prop_abToListEither_2 a b = (abToListEither $ a :/ b :/ ABNil) == ([Left a, Right b])
+
+prop_abToListEither_3 :: Int -> String -> Int -> Bool
+prop_abToListEither_3 a b c = (abToListEither $ a :/ b :/ c :/ ABNil) == ([Left a, Right b, Left c])
+
+prop_abToListEither_4 :: Int -> String -> Int -> String -> Bool
+prop_abToListEither_4 a b c d = (abToListEither $ a :/ b :/ c :/ d :/ ABNil) == ([Left a, Right b, Left c, Right d])
+
+
+g_abFromListEither =
+  testGroup "abFromListEither"
+  [ testProperty "empty" prop_abFromListEither_empty
+  , testProperty "g" prop_abFromListEither_g
+  , testProperty "gg" prop_abFromListEither_gg
+  , testProperty "ggg" prop_abFromListEither_ggg
+  , testProperty "gggg" prop_abFromListEither_gggg
+  , testProperty "ggggg" prop_abFromListEither_gggg
+  , testProperty "b" prop_abFromListEither_b
+  , testProperty "gb" prop_abFromListEither_gb
+  , testProperty "ggb" prop_abFromListEither_gb
+  ]
+
+prop_abFromListEither_empty :: Bool
+prop_abFromListEither_empty = abFromListEither ([] :: [Either Int String]) == Just ABNil
+
+prop_abFromListEither_g :: Int -> Bool
+prop_abFromListEither_g a = abFromListEither ([Left a] :: [Either Int String]) == Just (a :/ ABNil)
+
+prop_abFromListEither_gg :: Int -> Float -> Bool
+prop_abFromListEither_gg a b = abFromListEither ([Left a, Right b]) == Just (a :/ b :/ ABNil)
+
+prop_abFromListEither_ggg :: Int -> Float -> Int -> Bool
+prop_abFromListEither_ggg a b c = abFromListEither [Left a, Right b, Left c] == Just (a :/ b :/ c :/ ABNil)
+
+prop_abFromListEither_gggg :: Int -> Float -> Int -> Float -> Bool
+prop_abFromListEither_gggg a b c d = abFromListEither [Left a, Right b, Left c, Right d] == Just (a :/ b :/ c :/ d :/ ABNil)
+
+prop_abFromListEither_ggggg :: Int -> Float -> Int -> Float -> Int -> Bool
+prop_abFromListEither_ggggg a b c d e = abFromListEither [Left a, Right b, Left c, Right d] == Just (a :/ b :/ c :/ d :/ e :/ ABNil)
+
+prop_abFromListEither_b :: String -> Bool
+prop_abFromListEither_b a = abFromListEither ((Right a : undefined) :: [Either Int String]) == Nothing
+
+prop_abFromListEither_gb :: Bool
+prop_abFromListEither_gb = abFromListEither ((Left undefined : Left undefined : undefined) :: [Either Int String]) == Nothing
+
+prop_abFromListEither_ggb :: String -> Char -> Bool
+prop_abFromListEither_ggb a b = abFromListEither (Left a : Right b : Left undefined : undefined) == Nothing
+
+
