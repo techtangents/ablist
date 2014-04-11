c0b408f324f2c4feef49092858b5746cd162847b
techtangents <dylan@techtangents.com>
2014-04-11 22:43:23 +1000

    abFoldr'

--- src/Data/ABList.hs
+++ src/Data/ABList.hs
@@ -63,12 +63,12 @@ aaFromList :: [a] -> ABList a a
 aaFromList = foldr (:/) ABNil
 
 abFoldr :: ((Either a b) -> t -> t) -> t -> (ABList a b) -> t
-abFoldr _ t ABNil = t
-abFoldr f t (a :/ ABNil) = f (Left a) t
-abFoldr f t (a :/ b :/ cs) = f (Left a) (f (Right b) (abFoldr f t cs))
+abFoldr f = abFoldr' (f . Left) (f . Right)
 
 abFoldr' :: (a -> t -> t) -> (b -> t -> t) -> t -> (ABList a b) -> t
-abFoldr' = undefined
+abFoldr' _ _ t ABNil = t
+abFoldr' f g t (a :/ ABNil) = f a t
+abFoldr' f g t (a :/ b :/ cs) = f a (g b (abFoldr' f g t cs))
 
 abZip :: [a] -> [b] -> ABList a b
 abZip [] _ = ABNil
--- test/Tests.hs
+++ test/Tests.hs
@@ -24,6 +24,7 @@ tests =
     g_abFromPairs,
     g_abToPairs,
     g_abFoldr,
+    g_abFoldr',
     g_abMap,
     g_abMerge
   ]
@@ -265,8 +266,6 @@ g_abFoldr =
   , testProperty "list" p_abFoldr_list
   ]
 
--- abFoldr :: ((Either a b) -> t -> t) -> t -> (ABList a b) -> t
-
 p_abFoldr_0 :: Bool
 p_abFoldr_0 = abFoldr undefined "q" (ABNil :: ABList Int String) == "q"
 
@@ -278,6 +277,27 @@ p_abFoldr_2 a b = abFoldr (\e t -> (either show show e) ++ t) "q" (a :/ b :/ ABN
 
 p_abFoldr_list :: [Int] -> Bool
 p_abFoldr_list as = abFoldr (\e t -> (either show show e) ++ t) "q" (aaFromList as) == foldr (\e t -> show e ++ t) "q" as
+
+
+g_abFoldr' =
+  testGroup "abFoldr"
+  [ testProperty "0" p_abFoldr_0'
+  , testProperty "1" p_abFoldr_1'
+  , testProperty "2" p_abFoldr_2'
+  , testProperty "list" p_abFoldr_list'
+  ]
+
+p_abFoldr_0' :: Bool
+p_abFoldr_0' = abFoldr' undefined undefined "q" (ABNil :: ABList Int String) == "q"
+
+p_abFoldr_1' :: Int -> Bool
+p_abFoldr_1' a = abFoldr' (\z t -> show z ++ t) undefined "q" (a :/ ABNil) == show a ++ "q"
+
+p_abFoldr_2' :: Int -> Char -> Bool
+p_abFoldr_2' a b = abFoldr' (\x t -> show x ++ t) (\x t -> show x ++ t) "q" (a :/ b :/ ABNil) == show a ++ show b ++ "q"
+
+p_abFoldr_list' :: [Int] -> Bool
+p_abFoldr_list' as = abFoldr' (\x t -> show x ++ t) (\x t -> show x ++ t) "q" (aaFromList as) == foldr (\e t -> show e ++ t) "q" as
 
 
 g_abMap =
