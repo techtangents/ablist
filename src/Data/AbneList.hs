ddf73d9894ba6d5672d1c69503379bc715bf7e55
techtangents <dylan@techtangents.com>
2014-04-19 00:09:33 +1000

    abneHead, abneTail and some ideas for other ops

--- src/Data/AbneList.hs
+++ src/Data/AbneList.hs
@@ -5,3 +5,16 @@ import Data.AbList
 infixr 5 ://
 data AbneList a b = a :// AbList b a
 
+-- test
+abneHead :: AbneList a b -> a
+abneHead (a :// _) = a
+
+-- test
+abneTail :: AbneList a b -> AbList b a
+abneTail (_ :// as) = as
+
+-- fold
+-- reduce
+-- reverse
+-- map
+-- functor
